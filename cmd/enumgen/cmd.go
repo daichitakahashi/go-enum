f2ebc897e6564860d7e82db4779517f67be90ed7
daichitakahashi <t.daichi.snd@gmail.com>
2023-07-19 23:13:24 +0900

    Update enumgen: add "-wd" and "-out" option

--- cmd/enumgen/cmd.go
+++ cmd/enumgen/cmd.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/daichitakahashi/go-enum/cmd/enumgen/gen"
 )
 
+var (
+	wd  = flag.String("wd", ".", "working directory")
+	out = flag.String("out", "enum.gen.go", "output file name")
+)
+
 func main() {
-	gen.Run("./user", "enum.gen.go")
+	flag.Parse()
+	gen.Run(*wd, *out)
 }
